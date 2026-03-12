16da5ddc49fe2eb1e0c3fdee11d49f47b0cafb0b
noregularexpressions <noregexp@gmail.com>
2026-03-12 01:46:50 -0400

    Update changelogs.lua

--- boxten sex gui/setup/script sections/changelogs.lua
+++ boxten sex gui/setup/script sections/changelogs.lua
@@ -9,18 +9,11 @@
    
 ---------------------------------------------------------------------------------------------------------------------------]]--
 
-local version = 3
+local version = 4
 
 -------------------------------------------------------------------------------------------------------------------------------
 
 -- services & instances
-local t, spwn = task.wait, task.spawn
-local getmmfromerr = function(userdata, f, test) local ret = nil xpcall(f, function() ret = debug.info(2, "f") end, userdata, nil, 0) if (type(ret) ~= "function") or not test(ret) then return f end return ret end
-local randstring = function() local s = "" for i = 1, math.random(8, 15) do if math.random(2) == 2 then s = s .. string.char(math.random(65, 90)) else s = s .. string.char(math.random(97, 122)) end end return s end
-
-local getins = getmmfromerr(game, function(a,b) return a[b] end, function(f) local a = Instance.new("Folder") local b = randstring() a.Name = b return f(a, "Name") == b end)
-local FindFirstChildOfClass = getins(game, "FindFirstChildOfClass") 
-
 local getgenv = getgenv() or _G
 
 local folder = "Bоxten Sеx GUI"
